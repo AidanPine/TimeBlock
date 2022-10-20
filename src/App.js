248fc4e676a359adede8c75d01bc498da8849e92
evanbrooks0629 <evanbrooks0629@gmail.com>
2022-10-20 12:16:21 -0400

    added router w/ home, dashboard
    Co-authored-by: NoireBlanc <NoireBlanc@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-function App() {
-    return (
-        <div className="App">
-            <h3>Empty Create React App</h3>
-            <p>Hello World!</p>
-        </div>
-    );
-}
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Dashboard from './Dashboard';
+import Home from './Home';
 
+function App() {
+ return (
+    <Router>
+       <Routes>
+            <Route path="/TimeBlock" element={<Home />} />
+            <Route path="/TimeBlock/dashboard" element={<Dashboard />} />
+       </Routes>
+    </Router>
+ );
+}
 export default App;
---
+++ src/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <h3>Dashboard</h3>
+        </div>
+    );
+}
+
+export default Dashboard;
