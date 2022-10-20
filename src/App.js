0e0589169b2993723b1b51bc89dd9ab8cc2e1013
evanbrooks0629 <60624108+evanbrooks0629@users.noreply.github.com>
2022-10-20 11:47:55 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div className="App">
+            <h3>Empty Create React App</h3>
+            <p>Hello World!</p>
+        </div>
+    );
+}
+
+export default App;
